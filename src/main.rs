4e59b0c02d09e71370103e275e774e3d37d25a9c
Zach <ZachRichDev@gmail.com>
2024-10-31 15:51:38 -0400

    Servo Operation Baseline
    - Implemented move and list servos

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 mod move_request;
 mod pca9685_controller;
+mod servo_info;
 
 use crate::move_request::move_request::MoveRequest;
 use crate::pca9685_controller::Pca9685Controller;
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use pwm_pca9685::Channel;
-
-/*// Constants for pulse widths
-const MIN_PULSE: u16 = 150; // Approx 1 ms pulse width (minimum position)
-const CENTER_PULSE: u16 = 375; // Approx 1.5 ms pulse width (center position)
-const MAX_PULSE: u16 = 600; // Approx 2 ms pulse width (maximum position)*/
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -20,31 +16,49 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(controller.clone()))
-            .route("/move", web::post().to(move_servo_handler))
+            .route("/servos", web::get().to(get_available_servos_handler))
+            .route("/servos/move", web::post().to(move_servo_handler))
     })
     .bind("0.0.0.0:3030")?
     .run()
     .await
 }
 
-// Async handler function to move a servo based on the API request
+// Handler function to get all available servos
+async fn get_available_servos_handler(
+    controller: web::Data<Pca9685Controller>,
+) -> impl Responder {
+    let servos = controller.get_available_servos();
+    HttpResponse::Ok().json(servos)
+}
+
 async fn move_servo_handler(
     body: web::Json<MoveRequest>,
     controller: web::Data<Pca9685Controller>,
 ) -> impl Responder {
-
-    if body.angle > 90 {
+    let angle = body.angle;
+    if angle > 90 {
         return HttpResponse::BadRequest().body("Invalid angle. Servo supports 0° to 90°.");
     }
 
-    let pulse_width = angle_to_pulse(body.angle);
+    // Look up the servo by name
+    let servo_name = &body.servo_name;
+    let servo_info = controller.get_servo_by_name(servo_name);
+    let servo_info = match servo_info {
+        Some(info) => info,
+        None => return HttpResponse::BadRequest().body("Servo not found"),
+    };
 
-    let channel = match body.channel {
+    let pulse_width = angle_to_pulse(angle);
+
+    // Convert the channel number to `Channel` enum
+    let channel = match servo_info.channel {
         0 => Channel::C0,
         1 => Channel::C1,
         2 => Channel::C2,
         3 => Channel::C3,
         4 => Channel::C4,
+        // Add more channels as needed
         _ => return HttpResponse::BadRequest().body("Invalid channel"),
     };
 
