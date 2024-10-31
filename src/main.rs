b289d1e4d4e88b530b2faf75524685d004978288
Zach <ZachRichDev@gmail.com>
2024-10-30 20:02:00 -0400

    Endpoint - Get Servos
    - Added logging
    - Updated Get Servos to return a list of strings

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ mod i2c_manager;
 
 use actix_web::{web, App, HttpServer};
 use std::sync::{Arc, Mutex};
+use log::{info, debug, error};
+use env_logger;
 use crate::i2c_manager::I2cManager;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
     // Create I2cManager wrapped in Arc<Mutex<...>> for thread-safe access
     let i2c_manager = Arc::new(Mutex::new(I2cManager::new()));
 
@@ -25,6 +28,6 @@ async fn main() -> std::io::Result<()> {
 
 async fn get_servos(i2c_manager: web::Data<Arc<Mutex<I2cManager>>>) -> String {
     let manager = i2c_manager.lock().unwrap();
-    manager.list_servos();
-    "Servos list".to_string()
+    info!("Getting servos...");
+    manager.list_servos()
 }
